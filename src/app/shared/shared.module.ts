11320f77176cc8a3ebd2c2fda0fb8354692a644e
mayrez <maikarox@protonmail.com>
2023-05-10 16:27:06 +0100

    Export new components in the shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,27 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from './components/button/button.component';
 import { RouterModule } from '@angular/router';
-
-
+import { StringArrayIteratorComponent } from './components/string-array-iterator/string-array-iterator.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoxComponent } from './components/box/box.component';
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    BoxComponent,
+    ButtonComponent,
+    HeaderComponent,
+    StringArrayIteratorComponent,
   ],
   imports: [
     CommonModule,
     MatButtonModule,
     RouterModule
   ],
-  exports: [ButtonComponent]
+  exports: [
+    BoxComponent,
+    ButtonComponent,
+    HeaderComponent,
+    StringArrayIteratorComponent,
+  ]
 })
 export class SharedModule { }
